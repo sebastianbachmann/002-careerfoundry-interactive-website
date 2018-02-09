d20591914e7493831a47c8d17ff2da6779ad99a6
Sebastian Bachmann <github@sebastianbachmann.de>
2018-02-09 22:34:19 +0100

    🤓 -  contains ALL TEH WORK

---
+++ js/work.js
@@ -0,0 +1,25 @@
+var works = [
+{
+  title: 'Heisenberg',
+  description: 'No speeches. Short speech. You lost your partner today. Whats his name - Emilio? Emilio is going to prison. The DEA took all your money, your lab. You got nothing',
+  url: '#',
+  link: 'http://www.animal-photography.com/thumbs/red_tabby_long_hair_kitten_~AP-0UJFTC-TH.jpg'
+},
+{
+  title: 'Pinkman',
+  description: 'Four pounds... foooour pounds as if two pounds wasnt bad enough. were talking two three hundred boxes of sinus pills there aint that many Smurfs in the world. So no ',
+  url: '#',
+  link: 'http://www.animal-photography.com/thumbs/red_tabby_long_hair_kitten_~AP-0UJFTC-TH.jpg'
+},
+{
+  title: 'Schrader',
+  description: 'Sorry, buddy. No can do. Pain is my foot in your ass. Ooh, heyo... pool party! Hey, I dont get the gag, jackoff. Who is this? So...',
+  url: '#',
+  link: 'http://www.animal-photography.com/thumbs/silver_tabby_kittens~AP-0JO6Y9-TH.jpg'
+},
+{
+  title: 'Godman',
+  description: 'Todays your lucky day. Look around, kiddo - its all yours. You are now the owner of this fine establishment. Free? Oh ladies, cover your ears. No... not free. Look,',
+  url: '#',
+  link: 'http://www.animal-photography.com/thumbs/silver_tabby_kitten_looking_up~AP-0DLVMB-TH.jpg'
+}]
